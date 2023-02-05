cafd75aa0f81706214fa9cf35b02924e474dc9e3
Patrick W <Chelyocarpus@hotmail.com>
2023-02-05 03:53:40 +0100

    Update Book of Wisdom.filter
    - Added utility flasks
    - Changed ItemLevel to AreaLevel in progression
    - Added python script to copy updated file over

---
+++ copy_1.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+file1 = "D:/Path-of-Exile-Filter-Backup/Book of Wisdom.filter"
+file2 = "C:/Users/Goliath/Documents/My Games/Path of Exile/Book of Wisdom.filter"
+
+# Vergleiche die Änderungszeiten der beiden Dateien
+#if os.path.getmtime(file1) < os.path.getmtime(file2):
+    # Kopiere die neuere Datei in den Ordner
+    #shutil.copy2(file2, file1)
+    #print(f"{file2} was copied to {file1}")
+if os.path.getmtime(file1) > os.path.getmtime(file2):
+    # Kopiere die ältere Datei in den Ordner
+    shutil.copy2(file1, file2)
+    print(f"{file1} was copied to {file2}")
+else:
+    # Keine der beiden Dateien ist neuer als die andere
+    raise ValueError("Both files have the same modification time")
